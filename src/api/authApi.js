b8237fcb0cc781885aa999b5132d4c848d835bd8
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2025-03-21 12:28:24 +0200

    Implement registration

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -37,8 +37,24 @@ export const useLogin = () => {
 };
 
 export const useRegister = () => {
-    const register = (email, password) =>
-        axiosInstance.post(`user/register`, {email, password});
+    const register = (first_name, last_name, email, password, password_confirmation) =>
+        axiosInstance.post(`user/register`, {first_name, last_name, email, password, password_confirmation})
+        .then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            if (error.response) {
+                return error.response.data;
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+        });
 
     return {
         register,
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,9 +1,22 @@
 import {Link, useNavigate} from "react-router";
-import {useEffect} from "react";
+import {useActionState, useContext, useEffect, useState} from "react";
 import {addBodyClass, removeBodyClass} from "../../utils/helpers.js";
+import {UserContext} from "../../contexts/UserContext.jsx";
+import {useRegister} from "../../api/authApi.js";
 
-export default function Register(onRegister) {
+export default function Register() {
+    const [formValues, setFormValues] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+    });
+
+    const [registerErrors, setRegisterErrors] = useState({});
     const navigate = useNavigate();
+    const {userLoginHandler} = useContext(UserContext);
+    const {register} = useRegister();
 
     useEffect(() => {
         addBodyClass('register-page');
@@ -15,14 +28,37 @@ export default function Register(onRegister) {
         };
     }, []);
 
-    const registerAction = (formData) => {
-        const email = formData.get('email');
-        const password = formData.get('password');
+    const registerHandler = async (_, formData) => {
+        setRegisterErrors({});
+        const values = Object.fromEntries(formData);
+        setFormValues(values);
 
-        onRegister(email, password);
+        const authData = await register(
+            values.first_name,
+            values.last_name,
+            values.email,
+            values.password,
+            values.password_confirmation,
+        );
 
-        navigate('/home');
+        if (authData.status === 'error') {
+            if (authData.details) {
+                setRegisterErrors(authData.details);
+            } else {
+                alert(authData.message);
+            }
+
+            return;
+        }
+
+        authData.isLoggedIn = true;
+
+        userLoginHandler(authData);
+
+        navigate('/dashboard');
     }
+
+    const [_, registerAction, isPending] = useActionState(registerHandler, {email: '', password: ''});
 
     return (
         <div className="register-box">
@@ -34,16 +70,84 @@ export default function Register(onRegister) {
                     <p className="register-box-msg">Register a new membership</p>
                     <form action={registerAction}>
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Full Name"/>
+                            <input
+                                type="text"
+                                name="first_name"
+                                value={formValues.first_name}
+                                onChange={(e) => setFormValues({...formValues, first_name: e.target.value})}
+                                className={`form-control${registerErrors.first_name ? ' is-invalid' : ''}`}
+                                placeholder="First Name"
+                            />
                             <div className="input-group-text"><span className="bi bi-person"></span></div>
+                            {registerErrors.first_name &&
+                                <span className="invalid-feedback" role="alert">
+                                <strong>{registerErrors.first_name}</strong>
+                            </span>
+                            }
                         </div>
                         <div className="input-group mb-3">
-                            <input type="email" className="form-control" placeholder="Email"/>
+                            <input
+                                type="text"
+                                name="last_name"
+                                value={formValues.last_name}
+                                onChange={(e) => setFormValues({...formValues, last_name: e.target.value})}
+                                className={`form-control${registerErrors.last_name ? ' is-invalid' : ''}`}
+                                placeholder="Last Name"
+                            />
+                            <div className="input-group-text"><span className="bi bi-person"></span></div>
+                            {registerErrors.last_name &&
+                                <span className="invalid-feedback" role="alert">
+                                <strong>{registerErrors.last_name}</strong>
+                            </span>
+                            }
+                        </div>
+                        <div className="input-group mb-3">
+                            <input
+                                type="email"
+                                name="email"
+                                value={formValues.email}
+                                onChange={(e) => setFormValues({...formValues, email: e.target.value})}
+                                className={`form-control${registerErrors.email ? ' is-invalid' : ''}`}
+                                placeholder="Email"
+                            />
                             <div className="input-group-text"><span className="bi bi-envelope"></span></div>
+                            {registerErrors.email &&
+                                <span className="invalid-feedback" role="alert">
+                                <strong>{registerErrors.email}</strong>
+                            </span>
+                            }
                         </div>
                         <div className="input-group mb-3">
-                            <input type="password" className="form-control" placeholder="Password"/>
+                            <input
+                                type="password"
+                                name="password"
+                                value={formValues.password}
+                                onChange={(e) => setFormValues({...formValues, password: e.target.value})}
+                                className={`form-control${registerErrors.password ? ' is-invalid' : ''}`}
+                                placeholder="Password"
+                            />
                             <div className="input-group-text"><span className="bi bi-lock-fill"></span></div>
+                            {registerErrors.password &&
+                                <span className="invalid-feedback" role="alert">
+                                <strong>{registerErrors.password}</strong>
+                            </span>
+                            }
+                        </div>
+                        <div className="input-group mb-3">
+                            <input
+                                type="password"
+                                name="password_confirmation"
+                                value={formValues.password_confirmation}
+                                onChange={(e) => setFormValues({...formValues, password_confirmation: e.target.value})}
+                                className={`form-control${registerErrors.password_confirmation ? ' is-invalid' : ''}`}
+                                placeholder="Password Confirmation"
+                            />
+                            <div className="input-group-text"><span className="bi bi-lock-fill"></span></div>
+                            {registerErrors.password_confirmation &&
+                                <span className="invalid-feedback" role="alert">
+                                <strong>{registerErrors.password_confirmation}</strong>
+                            </span>
+                            }
                         </div>
                         <div className="row">
                             <div className="col-8">
@@ -56,14 +160,14 @@ export default function Register(onRegister) {
                             </div>
                             <div className="col-4">
                                 <div className="d-grid gap-2">
-                                    <button type="submit" className="btn btn-primary">Sign In</button>
+                                    <button type="submit" className="btn btn-primary" disabled={isPending}>Sign In</button>
                                 </div>
                             </div>
                         </div>
                     </form>
 
                     <p className="mb-0">
-                       <Link to="/login">I already have a membership</Link>
+                        <Link to="/login">I already have a membership</Link>
                     </p>
                 </div>
             </div>
