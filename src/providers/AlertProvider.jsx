01729edec8e51fc3da7e4bdbc9b4ebe7840aa900
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2025-03-21 23:50:12 +0200

    Remove log

--- src/providers/AlertProvider.jsx
+++ src/providers/AlertProvider.jsx
@@ -3,7 +3,6 @@ import {AlertContext} from "../contexts/AlertContext.jsx";
 
 export const AlertProvider = ({ children }) => {
     const [alert, setAlert] = useState({}); // State to hold alert data
-console.log("Current Alert State:", alert);
 
     return (
         <AlertContext.Provider value={{ alert, setAlert }}>
