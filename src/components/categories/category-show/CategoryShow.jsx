3b177f8c5471a153d29b836bbe8ab429faeae760
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2025-03-31 11:24:51 +0300

    Remove obsolete code

--- src/components/categories/category-show/CategoryShow.jsx
+++ src/components/categories/category-show/CategoryShow.jsx
@@ -1,17 +1,12 @@
 import React, {useEffect, useState} from "react";
 import AdminPanelPage from "../../../layouts/admin-panel-page/AdminPanelPage";
 import {useApiClient} from "../../../hooks/useApiClient.js";
-import DataTable from 'datatables.net-react';
-import DT from 'datatables.net-bs5';
-import "datatables.net-rowgroup-bs5";
 import {Link, useNavigate, useParams} from "react-router";
 import {getIncomeExpenseColorClassFromType} from "../../../utils/helpers.js";
 import CategoryNameAndIcon from "../category-name-and-icon/CategoryNameAndIcon.jsx";
 import Modal from "react-bootstrap/Modal";
 import {Button} from "react-bootstrap";
 import {useAlert} from "../../../contexts/AlertContext.jsx";
-
-DataTable.use(DT);
 
 export default function CategoryShow() {
     const {categoryId} = useParams();
