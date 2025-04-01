08ab9d52929e8ef5a21f3092d86eae1cc3a0dd80
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2025-04-01 16:36:39 +0300

    Refactor Register component to use React-Bootstrap

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -3,6 +3,10 @@ import {useActionState, useContext, useEffect, useState} from "react";
 import {addBodyClass, removeBodyClass} from "@utils/helpers.js";
 import {UserContext} from "@contexts/UserContext.jsx";
 import {useRegister} from "@api/authApi.js";
+import {Card, CardBody, Col, FormCheck, FormControl, InputGroup, Row} from "react-bootstrap";
+import FormCheckInput from "react-bootstrap/FormCheckInput";
+import FormCheckLabel from "react-bootstrap/FormCheckLabel";
+import InputGroupText from "react-bootstrap/InputGroupText";
 
 export default function Register() {
     const [formValues, setFormValues] = useState({
@@ -65,116 +69,134 @@ export default function Register() {
             <div className="register-logo">
                 <b>MyFinance</b>
             </div>
-            <div className="card">
-                <div className="card-body register-card-body">
+            <Card>
+                <CardBody>
                     <p className="register-box-msg">Register a new membership</p>
                     <form action={registerAction}>
-                        <div className="input-group mb-3">
-                            <input
-                                type="text"
-                                name="first_name"
-                                value={formValues.first_name}
-                                onChange={(e) => setFormValues({...formValues, first_name: e.target.value})}
-                                className={`form-control${registerErrors.first_name ? ' is-invalid' : ''}`}
-                                placeholder="First Name"
-                                required
-                            />
-                            <div className="input-group-text"><span className="bi bi-person"></span></div>
-                            {registerErrors.first_name &&
-                                <span className="text-danger" role="alert">
-                                <strong>{registerErrors.first_name}</strong>
-                            </span>
-                            }
-                        </div>
-                        <div className="input-group mb-3">
-                            <input
-                                type="text"
-                                name="last_name"
-                                value={formValues.last_name}
-                                onChange={(e) => setFormValues({...formValues, last_name: e.target.value})}
-                                className={`form-control${registerErrors.last_name ? ' is-invalid' : ''}`}
-                                placeholder="Last Name"
-                            />
-                            <div className="input-group-text"><span className="bi bi-person"></span></div>
-                            {registerErrors.last_name &&
-                                <span className="text-danger" role="alert">
-                                <strong>{registerErrors.last_name}</strong>
-                            </span>
-                            }
-                        </div>
-                        <div className="input-group mb-3">
-                            <input
-                                type="email"
-                                name="email"
-                                value={formValues.email}
-                                onChange={(e) => setFormValues({...formValues, email: e.target.value})}
-                                className={`form-control${registerErrors.email ? ' is-invalid' : ''}`}
-                                placeholder="Email"
-                                required
-                            />
-                            <div className="input-group-text"><span className="bi bi-envelope"></span></div>
-                            {registerErrors.email &&
-                                <span className="text-danger" role="alert">
-                                <strong>{registerErrors.email}</strong>
-                            </span>
-                            }
-                        </div>
-                        <div className="input-group mb-3">
-                            <input
-                                type="password"
-                                name="password"
-                                value={formValues.password}
-                                onChange={(e) => setFormValues({...formValues, password: e.target.value})}
-                                className={`form-control${registerErrors.password ? ' is-invalid' : ''}`}
-                                placeholder="Password"
-                                required
-                            />
-                            <div className="input-group-text"><span className="bi bi-lock-fill"></span></div>
-                            {registerErrors.password &&
-                                <span className="text-danger" role="alert">
-                                <strong>{registerErrors.password}</strong>
-                            </span>
-                            }
-                        </div>
-                        <div className="input-group mb-3">
-                            <input
-                                type="password"
-                                name="password_confirmation"
-                                value={formValues.password_confirmation}
-                                onChange={(e) => setFormValues({...formValues, password_confirmation: e.target.value})}
-                                className={`form-control${registerErrors.password_confirmation ? ' is-invalid' : ''}`}
-                                placeholder="Password Confirmation"
-                                required
-                            />
-                            <div className="input-group-text"><span className="bi bi-lock-fill"></span></div>
-                            {registerErrors.password_confirmation &&
-                                <span className="text-danger" role="alert">
-                                <strong>{registerErrors.password_confirmation}</strong>
-                            </span>
-                            }
-                        </div>
-                        <div className="row">
-                            <div className="col-8">
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" required/>
-                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        <Row>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        name="first_name"
+                                        value={formValues.first_name}
+                                        onChange={(e) => setFormValues({...formValues, first_name: e.target.value})}
+                                        className={registerErrors.first_name ? ' is-invalid' : ''}
+                                        placeholder="First Name"
+                                        required
+                                    />
+                                    <InputGroupText><i className="bi bi-person"></i></InputGroupText>
+                                    {registerErrors.first_name &&
+                                        <span className="text-danger" role="alert">
+                                            <strong>{registerErrors.first_name}</strong>
+                                        </span>
+                                    }
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        name="last_name"
+                                        value={formValues.last_name}
+                                        onChange={(e) => setFormValues({...formValues, last_name: e.target.value})}
+                                        className={registerErrors.last_name ? ' is-invalid' : ''}
+                                        placeholder="Last Name"
+                                    />
+                                    <InputGroupText><i className="bi bi-person"></i></InputGroupText>
+                                    {registerErrors.last_name &&
+                                        <span className="text-danger" role="alert">
+                                            <strong>{registerErrors.last_name}</strong>
+                                        </span>
+                                    }
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        type="email"
+                                        name="email"
+                                        value={formValues.email}
+                                        onChange={(e) => setFormValues({...formValues, email: e.target.value})}
+                                        className={registerErrors.email ? ' is-invalid' : ''}
+                                        placeholder="Email"
+                                        required
+                                    />
+                                    <InputGroupText><i className="bi bi-envelope"></i></InputGroupText>
+                                    {registerErrors.email &&
+                                        <span className="text-danger" role="alert">
+                                            <strong>{registerErrors.email}</strong>
+                                        </span>
+                                    }
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        type="password"
+                                        name="password"
+                                        value={formValues.password}
+                                        onChange={(e) => setFormValues({...formValues, password: e.target.value})}
+                                        className={registerErrors.password ? ' is-invalid' : ''}
+                                        placeholder="Password"
+                                        required
+                                    />
+                                    <InputGroupText><i className="bi bi-lock-fill"></i></InputGroupText>
+                                    {registerErrors.password &&
+                                        <span className="text-danger" role="alert">
+                                            <strong>{registerErrors.password}</strong>
+                                        </span>
+                                    }
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        type="password"
+                                        name="password_confirmation"
+                                        value={formValues.password_confirmation}
+                                        onChange={(e) => setFormValues({...formValues, password_confirmation: e.target.value})}
+                                        className={registerErrors.password_confirmation ? ' is-invalid' : ''}
+                                        placeholder="Password Confirmation"
+                                        required
+                                    />
+                                    <InputGroupText><i className="bi bi-lock-fill"></i></InputGroupText>
+                                    {registerErrors.password_confirmation &&
+                                        <span className="text-danger" role="alert">
+                                            <strong>{registerErrors.password_confirmation}</strong>
+                                        </span>
+                                    }
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm={8}>
+                                <FormCheck>
+                                    <FormCheckInput name="terms" value="" id="flexCheckDefault" required/>
+                                    <FormCheckLabel htmlFor="terms">
                                         I agree to the <Link to={"/terms"}>terms</Link>
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="col-4">
+                                    </FormCheckLabel>
+                                </FormCheck>
+                            </Col>
+                            <Col sm={4}>
                                 <div className="d-grid gap-2">
                                     <button type="submit" className="btn btn-primary" disabled={isPending}>Register</button>
                                 </div>
-                            </div>
-                        </div>
+                            </Col>
+                        </Row>
                     </form>
 
                     <p className="mb-0">
                         <Link to="/login">I already have a membership</Link>
                     </p>
-                </div>
-            </div>
+                </CardBody>
+            </Card>
         </div>
     )
 }
