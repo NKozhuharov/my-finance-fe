e0abf5d918a34e12aba504ccbdde2507b9e90b9c
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2025-04-02 19:14:33 +0300

    Always show user initials

--- src/components/user-menu/UserMenu.jsx
+++ src/components/user-menu/UserMenu.jsx
@@ -18,7 +18,7 @@ export default function UserMenu({firstName, lastName}) {
 
     return <li className="nav-item dropdown user-menu">
         <a href="#" onClick={showHandler} className={`nav-link dropdown-toggle${isShown ? ' show' : ''}`}>
-            <b className="d-none d-md-inline">{`${firstName[0]} ${lastName[0]}`}</b>
+            <b className="d-md-inline">{`${firstName[0]} ${lastName[0]}`}</b>
         </a>
         <ul className={`dropdown-menu dropdown-menu-lg dropdown-menu-end${isShown ? ' show' : ''}`} data-bs-popper="static">
             <li className="user-header text-bg-primary" style={{minHeight: "0"}}>
