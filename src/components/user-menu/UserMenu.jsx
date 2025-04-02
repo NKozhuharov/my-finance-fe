9777e6a12cb5870bd3707b14bdfe46208ec6cee9
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2025-04-02 23:15:46 +0300

    Refactor UserMenu to use react-bootstrap Dropdown

--- src/components/user-menu/UserMenu.jsx
+++ src/components/user-menu/UserMenu.jsx
@@ -1,26 +1,19 @@
-import {useState} from "react";
 import {Link} from "react-router";
-import {Button} from "react-bootstrap";
+import {Button, Dropdown} from "react-bootstrap";
 import {useLogout} from "@api/authApi.js";
 
 export default function UserMenu({firstName, lastName}) {
-    const [isShown, setIsShown] = useState(false);
-
     const {logout} = useLogout();
-
-    const showHandler = () => {
-        setIsShown(!isShown)
-    }
 
     const handleLogout = () => {
         logout();
     }
 
-    return <li className="nav-item dropdown user-menu">
-        <a href="#" onClick={showHandler} className={`nav-link dropdown-toggle${isShown ? ' show' : ''}`}>
+    return <Dropdown className="nav-item dropdown user-menu">
+        <Dropdown.Toggle className={`nav-link dropdown-toggle`} variant={"link"}>
             <b className="d-md-inline">{`${firstName[0]} ${lastName[0]}`}</b>
-        </a>
-        <ul className={`dropdown-menu dropdown-menu-lg dropdown-menu-end${isShown ? ' show' : ''}`} data-bs-popper="static">
+        </Dropdown.Toggle>
+        <Dropdown.Menu className={`dropdown-menu dropdown-menu-lg dropdown-menu-end`} data-bs-popper="static">
             <li className="user-header text-bg-primary" style={{minHeight: "0"}}>
                 {`${firstName} ${lastName}`}
             </li>
@@ -28,6 +21,6 @@ export default function UserMenu({firstName, lastName}) {
                 <Link to="/user-profile" className="btn btn-primary">Profile</Link>
                 <Button to="/logout" className="btn-danger float-end" onClick={handleLogout}>Sign out</Button>
             </li>
-        </ul>
-    </li>
+        </Dropdown.Menu>
+    </Dropdown>
 }
